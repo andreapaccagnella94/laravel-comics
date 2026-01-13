12006dfe9ee9e5332e0833b40423e2e656f900d9
andreapaccagnella94 <andreapacca94@live.it>
2026-01-13 19:40:17 +0100

    Enhance footer layout and card styling for improved UI

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,9 +1,9 @@
 <!-- An unexamined life is not worth living. - Socrates -->
 
-<div class="card h-100" style="width:11rem;">
+<div class="card h-100 bg-light-dark">
   <img src={{$link}} class="card-img-top" alt="immagine copertina">
   <div class="card-body">
     <h5 class="card-title">{{$titolo}}</h5>
-    <h6 class="card-subtitle mb-2 text-muted ">{{$serie}}</h6>
+    <h6 class="card-subtitle mb-2 text-muted">{{$serie}}</h6>
   </div>
 </div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,12 @@
 <footer>
+    <div class="links row">
+        <div class="left col-sm-12 col-md-6">
+
+        </div>
+        <div class="right col-sm-12 col-md-6">
+
+        </div>
+    </div>
     <div class="sing-up fixed-bottom">
         <h3 class="text-center mt-4">stiky?</h3>
     </div>
